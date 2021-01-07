6f0324e5d6ff43edb1779a7a5e2b51e447ab64fa
lfernando488 <lfernando488@gmail.com>
2021-01-07 13:40:16 -0300

    request api and product card

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import Footer from '../Footer';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 function Orders() {
+
+  const [producs, setproducts] = useState<Product[]>([]); 
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setproducts(response.data))
+      .catch(error => console.log("error"))
+  }, [])
+
   return (
     <div className="orders-container">
         <StepsHeader /> 
+        <ProductsList products={producs}/>
+        <Footer />
     </div>
   );
 }
